59de1b36d39004ca6fec0b9d55f32ed93f40a2b9
Sayanendu Pramanik <Sayanendurkmv5517@gmail.com>
2026-01-19 16:47:31 +0530

    change logic in payment repository

--- class_assignment/src/main/java/com/example/class_assignment/repository/PaymentRepository.java
+++ class_assignment/src/main/java/com/example/class_assignment/repository/PaymentRepository.java
@@ -2,7 +2,10 @@ package com.example.class_assignment.repository;
 
 import com.example.class_assignment.model.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface PaymentRepository extends MongoRepository<Payment, String> {
+
+    @Query("{ 'orderId' : ?0 }")
     Payment findByOrderId(String orderId);
 }
